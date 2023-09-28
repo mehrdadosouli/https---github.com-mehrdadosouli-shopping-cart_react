5b94f84011e90680ccfbb997860cb828230b9bf0
mehrdad <mehrdadosoulisaran@gmail.com>
2023-09-28 10:25:56 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Products from "./components/Products"
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Products />
+      </div>
+    )
+  }
+}
