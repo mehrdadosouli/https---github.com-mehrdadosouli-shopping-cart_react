aa98dcc7cf72b308920f87eccbe19c7c19c8e129
mehrdad <mehrdadosoulisaran@gmail.com>
2023-11-07 08:24:51 +0330

    change destructure total and countItem and set handler function in cardcontextprovider

--- src/components/BasketProducts.jsx
+++ src/components/BasketProducts.jsx
@@ -5,21 +5,22 @@ import {cardContext} from "../context/CardContextProvider"
 export default function BasketProducts() {
   const productSelected=useContext(cardContext)
   const {state,dispatch}=productSelected;
-  const total=state.selectedItem.map(item=>{
-    return (item.price * item.quantity)
-  })
-  let sum=0
-  const res=total.reduce((a,b)=>{
-    return a + b
-  },0)
-  state.total=res;
-  state.selectedItem.forEach(item=>{
-    console.log(item,'sum',sum);
-     sum=sum + item.quantity
-     return sum
-  })
-  state.countItem=sum
-  console.log(state.checkout);
+  console.log(state);
+  // const total=state.selectedItem.map(item=>{
+  //   return (item.price * item.quantity)
+  // })
+  // let sum=0
+  // const res=total.reduce((a,b)=>{
+  //   return a + b
+  // },0)
+  // state.total=res;
+  // state.selectedItem.forEach(item=>{
+  //   console.log(item,'sum',sum);
+  //    sum=sum + item.quantity
+  //    return sum
+  // })
+  // state.countItem=sum
+  // console.log(state.checkout);
   return (
     <div style={{display:'flex',width:'100%'}}>
       <div style={{width:'100%'}}>
@@ -53,11 +54,8 @@ export default function BasketProducts() {
       </div>
       <div className={styles.lefts}>
                 <div className={styles.left_container}>
-                  {
-                    state.checkout ? 
-                    <span><Link to='/products'>Back to Home</Link></span>
-                    :""
-                  }
+                <span><Link to='/products'>stores</Link></span>
+                  
                   <div className={styles.left_checkout}>
                     <div>
                       <span>all price : {state.total.toFixed(2)}</span>
@@ -69,6 +67,11 @@ export default function BasketProducts() {
                       <button onClick={()=>dispatch({type:'CLEAR'})}>clear</button>
                       <button onClick={()=>dispatch({type:'CHECKOUT'})}>checkOut</button>
                     </div>
+                    {
+                    !state.checkout ? 
+                    <span><Link to='/products'>Back to Home</Link></span>
+                    :""
+                  }
                   </div>
                 </div>
         </div>
--- src/context/CardContextProvider.jsx
+++ src/context/CardContextProvider.jsx
@@ -6,39 +6,55 @@ const initialState={
     checkout:false,
     countItem:0
 }
+const handler=(item)=>{
 
+    const countItem=item.reduce((first,product)=>{
+        return first + product.quantity
+    },0)
+    const total=item.reduce((items,product)=>{
+        return items + product.price * product.quantity
+    },0)
+    return {total,countItem}
+}
 const productReducer=(state,action) => {
     switch (action.type) {
         case "ADD_ITEM":
             if(!state.selectedItem.find(item=>item.id === action.payload.id )){
                 state.selectedItem.push({
                     ...action.payload,
-                    quantity:1,
-                    checkout:false
+                    quantity:1
                 })
+                
             }
             return {
                 ...state,
                 selectedItem:state.selectedItem,
-                checkout:false
+                checkout:false,
+                ...handler(state.selectedItem)
             }
             case "DELETE_ITEM" :
                 const result= state.selectedItem.filter(item=>item.id !== action.payload.id)
             return {
                     ...state,
                     selectedItem:[...result],
+                    checkout:true,
+                    ...handler(result)
             }
             case "INCREASE" :
                     const indexI=state.selectedItem.findIndex(index=>index.id == action.payload.id);
                     state.selectedItem[indexI].quantity++;
-            return {...state}
+            return {...state,
+                checkout:true,
+                ...handler(state.selectedItem)}
             case "DECREASE" :
                 const indexD=state.selectedItem.findIndex(index=>index.id == action.payload.id);
                 if(state.selectedItem[indexD].quantity > 0){
                     state.selectedItem[indexD].quantity--;
                 }
                 
-            return {...state}
+            return {...state,
+                checkout:true,
+            ...handler(state.selectedItem)}
             case "CHECKOUT" :
                 if(state.checkout){
                     alert('checkout is successfully')
